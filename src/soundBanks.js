5c6de3557917fefd3ea469a71eb04eefd212b825
Dam-C <dams.c128@gmail.com>
2023-08-29 18:47:56 +0200

    testing

--- src/soundBanks.js
+++ src/soundBanks.js
@@ -4,12 +4,12 @@ export const soundBanks = {
       {
         name: "attack",
         keypad: "Q",
-        link: "../src/assets/soundbank-start/attack.mp3",
+        link: "./assets/soundbank-start/attack.mp3",
       },
       {
         name: "blip",
         keypad: "W",
-        link: "../src//assets/soundbank-start/blip.mp3",
+        link: "../src/assets/soundbank-start/blip.mp3",
       },
       {
         name: "blow",
@@ -19,32 +19,32 @@ export const soundBanks = {
       {
         name: "cursor-move",
         keypad: "A",
-        link: "../src//assets/soundbank-start/cursor-move.mp3",
+        link: "../src/assets/soundbank-start/cursor-move.mp3",
       },
       {
         name: "explosion",
         keypad: "S",
-        link: "../src//assets/soundbank-start/explosion.mp3",
+        link: "../src/assets/soundbank-start/explosion.mp3",
       },
       {
         name: "healthup",
         keypad: "D",
-        link: "../src//assets/soundbank-start/healthup.mp3",
+        link: "../src/assets/soundbank-start/healthup.mp3",
       },
       {
         name: "hit",
         keypad: "Z",
-        link: "../src//assets/soundbank-start/hit.mp3",
+        link: "../src/assets/soundbank-start/hit.mp3",
       },
       {
         name: "power-up",
         keypad: "X",
-        link: "../src//assets/soundbank-start/power-up.mp3",
+        link: "../src/assets/soundbank-start/power-up.mp3",
       },
       {
         name: "trampoline",
         keypad: "C",
-        link: "../src//assets/soundbank-start/trampoline.mp3",
+        link: "../src/assets/soundbank-start/trampoline.mp3",
       },
     ],
   },
@@ -58,7 +58,7 @@ export const soundBanks = {
       {
         name: "blip",
         keypad: "W",
-        link: "../src//assets/soundbank-start/blip.mp3",
+        link: "../src/assets/soundbank-start/blip.mp3",
       },
       {
         name: "blow",
@@ -68,32 +68,32 @@ export const soundBanks = {
       {
         name: "cursor-move",
         keypad: "A",
-        link: "../src//assets/soundbank-start/cursor-move.mp3",
+        link: "../src/assets/soundbank-start/cursor-move.mp3",
       },
       {
         name: "explosion",
         keypad: "S",
-        link: "../src//assets/soundbank-start/explosion.mp3",
+        link: "../src/assets/soundbank-start/explosion.mp3",
       },
       {
         name: "healthup",
         keypad: "D",
-        link: "../src//assets/soundbank-start/healthup.mp3",
+        link: "../src/assets/soundbank-start/healthup.mp3",
       },
       {
         name: "hit",
         keypad: "Z",
-        link: "../src//assets/soundbank-start/hit.mp3",
+        link: "../src/assets/soundbank-start/hit.mp3",
       },
       {
         name: "power-up",
         keypad: "X",
-        link: "../src//assets/soundbank-start/power-up.mp3",
+        link: "../src/assets/soundbank-start/power-up.mp3",
       },
       {
         name: "trampoline",
         keypad: "C",
-        link: "../src//assets/soundbank-start/trampoline.mp3",
+        link: "../src/assets/soundbank-start/trampoline.mp3",
       },
     ],
   },
